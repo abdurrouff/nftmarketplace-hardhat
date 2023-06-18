f3668d140dc6691918b735a85f81dfc09a62724b
Rawman <rouffaderemi@gmail.com>
2023-06-18 15:48:58 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0x169e92dafA49c6D35F35C8826A37991eEF392718';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nftmarketplace-drab.vercel.app/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
